e0726af832f684c1e1ec91164bad6e58e8e04104
Állan Anderson <allan_brz@hotmail.com>
2023-02-15 19:44:01 -0300

    ScrollSuave em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,25 +1,37 @@
-export default function scrollToSection() {
-  const linkInternos = document.querySelectorAll(".js-menu a[href^='#'");
+export default class ScrollSection {
+  constructor(links, options) {
+    this.linkInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: 'smooth', block: 'start' };
+    } else {
+      this.options = options;
+    }
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
 
-  function scrollToSection1(event) {
+  scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
+  // Maneira alternativa
+  // const topo = section.offsetTop;
+  // window.scrollTo({
+  //   top: topo,
+  //   behavior: 'smooth',
+  // })
 
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
+  addLinkEvent() {
+    this.linkInternos.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
     });
-
-    // Maneira alternativa
-    // const topo = section.offsetTop;
-    // window.scrollTo({
-    //   top: topo,
-    //   behavior: 'smooth',
-    // })
   }
 
-  linkInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection1);
-  });
+  init() {
+    if (this.linkInternos.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
 }
--- js/script.js
+++ js/script.js
@@ -2,18 +2,20 @@ import initAccordion from './modules/accordion.js';
 import initAnimationScroll from './modules/animacao-scroll.js';
 import initDropDownMenu from './modules/dropdown-menu.js';
 import initModal from './modules/modal.js';
-import scrollToSection from './modules/scroll-suave.js';
+import ScrollSection from './modules/scroll-suave.js';
 import tabMenuActive from './modules/tab-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
+const scrollSection = new ScrollSection(".js-menu a[href^='#'");
+scrollSection.init();
+
 initAccordion();
 initAnimationScroll();
 initDropDownMenu();
 initModal();
-scrollToSection();
 tabMenuActive();
 initMenuMobile();
 initFuncionamento();
