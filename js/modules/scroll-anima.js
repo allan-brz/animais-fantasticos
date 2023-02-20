a94f8e2637d2766edab999462c054428bac324c9
Állan Anderson <allan_brz@hotmail.com>
2023-02-20 15:42:23 -0300

    refataração com class do scroll anima

---
+++ js/modules/scroll-anima.js
@@ -0,0 +1,24 @@
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
+
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const distY = section.getBoundingClientRect().top - this.windowMetade;
+      if (distY < 0) {
+        section.classList.add('ativo');
+      } else if (section.classList.contains('ativo')) {
+        section.classList.remove('ativo');
+      }
+    });
+  }
+
+  init() {
+    this.animaScroll();
+    window.addEventListener('scroll', this.animaScroll);
+  }
+}
